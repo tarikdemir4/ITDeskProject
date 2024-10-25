f035ad6ba2e489015728a7858429698845ff586b
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-10-25 22:31:31 +0300

    .

--- ITDesk.WebApi/Services/PasswordService.cs
+++ ITDesk.WebApi/Services/PasswordService.cs
@@ -7,9 +7,11 @@ public static class PasswordService
 {
     public static void CreatePassword(string password, out byte[] hash, out byte[] salt)
     {
+
         var hmac = new System.Security.Cryptography.HMACSHA512();
         salt = hmac.Key;
         hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
 
     }
     public static bool CheckPassword(User user, string password)
