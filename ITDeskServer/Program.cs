f1a318f56e3cb7067644cb81fa4ad40e98c908db
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-10-27 22:43:55 +0300

    AuthController yazıldı, şifre kontrol edildi.

---
+++ ITDeskServer/Program.cs
@@ -0,0 +1,54 @@
+using ITDeskServer.Context;
+using ITDeskServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer("Data Source=TARDEMPC\\SQLEXPRESS;Initial Catalog=ITDeskDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+});
+
+
+builder.Services.AddIdentityCore<AppUser>(options =>
+{
+    options.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scoped = app.Services.CreateScope())
+{
+    var userManager = scoped.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+    if (!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new()
+        {
+            Email = "test@test.com",
+            UserName = "test",
+            Name = "Tarýk",
+            LastName = "Demir"
+
+        }, "Password12*").Wait();
+    }
+}
+
+app.Run();
